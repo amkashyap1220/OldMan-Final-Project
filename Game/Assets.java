ecc88d92e4d61086540310abcdae3b24c81b0dc0
mlsanborn <mlsanborn@stu.naperville203.org>
2017-05-22 10:18:43 -0500

    ADGHSFUFgyiogfrsejnh

--- Game/Assets.java
+++ Game/Assets.java
@@ -10,18 +10,22 @@ public class Assets
     //Add all assets here
     public static BufferedImage 
     titlescreen, loadingscreen, deadscreen,
+    lvl1_1,lvl1_2,lvl1_3,lvl1_boss,
     player,
     idle1, idle2,
     walking1, walking2,
-    lvl1bg, lvl2bg, lvl3bg,
-    instructions,
+    throw1, throw2,
+    flipwalking1, flipwalking2,
+    flipthrow1, flipthrow2,
+    lvl1bg, lvl2bg, lvl3bg,bossbg,
+    instructions, nostory, credits,
     slime1, slime2,
     snake1, snake2,
     flipidle1, flipidle2,
     gameover,
-    bossbg,
-    cane1,
-    cane2,
+    cane1, cane2,
+    icon,
+    healthbar,
     boss;
     /**
      * Loads all of the images (called by the init() in game)
@@ -30,12 +34,22 @@ public class Assets
     {
         titlescreen = ImageLoader.loadImage("resources/images/title.png");
         loadingscreen = ImageLoader.loadImage("resources/images/loadingscreen.png");
+        lvl1_1 = ImageLoader.loadImage("resources/images/lvl1-1.png");
+        lvl1_2 = ImageLoader.loadImage("resources/images/lvl1-2.png");
+        lvl1_3 = ImageLoader.loadImage("resources/images/lvl1-3.png");
+        lvl1_boss = ImageLoader.loadImage("resources/images/lvl1-boss.png");
         deadscreen = ImageLoader.loadImage("resources/images/dead.png");
         player = ImageLoader.loadImage("resources/images/dalelewis.png");
         idle1 = ImageLoader.loadImage("resources/images/idleanim1.png");
         idle2 = ImageLoader.loadImage("resources/images/idleanim2.png");
         walking1 = ImageLoader.loadImage("resources/images/walking1.png");
         walking2 = ImageLoader.loadImage("resources/images/walking2.png");
+        flipwalking1 = ImageLoader.loadImage("resources/images/flipwalking1.png");
+        flipwalking2 = ImageLoader.loadImage("resources/images/flipwalking2.png");
+        throw1 = ImageLoader.loadImage("resources/images/throwanim1.png");
+        throw2 = ImageLoader.loadImage("resources/images/throwanim2.png");
+        flipthrow1 = ImageLoader.loadImage("resources/images/flipthrowanim1.png");
+        flipthrow2 = ImageLoader.loadImage("resources/images/flipthrowanim2.png");
         flipidle1 = ImageLoader.loadImage("resources/images/idleanimflip1.png");
         flipidle2 = ImageLoader.loadImage("resources/images/idleanimflip2.png");
         lvl1bg = ImageLoader.loadImage("resources/images/lvl1bg.png");
@@ -50,5 +64,9 @@ public class Assets
         snake2 = ImageLoader.loadImage("resources/images/snake2.png");
         boss = ImageLoader.loadImage("resources/images/mrhayes1.jpg");
         cane1 = ImageLoader.loadImage("resources/images/cane.png");
+        credits = ImageLoader.loadImage("resources/images/creditscreen.png");
+        nostory = ImageLoader.loadImage("resources/images/oopsnostory.png");
+        icon = ImageLoader.loadImage("resources/images/icon.png");
+        healthbar = ImageLoader.loadImage("resources/images/healthbartransparent.png");
     }
 }
