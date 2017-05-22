7bb20520a44eb35a59d8aec544e11386a60d6e7c
mlsanborn <mlsanborn@stu.naperville203.org>
2017-05-21 20:51:22 -0500

    what

--- Game/Assets.java
+++ Game/Assets.java
@@ -9,20 +9,16 @@ public class Assets
 {
     //Add all assets here
     public static BufferedImage 
-    titlescreen,
+    titlescreen, loadingscreen, deadscreen,
     player,
-    idle1,
-    idle2,
-    lvl1bg,
+    idle1, idle2,
+    walking1, walking2,
+    lvl1bg, lvl2bg, lvl3bg,
     instructions,
     slime1, slime2,
     flipidle1, flipidle2,
     gameover,
-    lvl2bg,
-    lvl3bg,
     bossbg,
-    loadingscreen,
-    deadscreen,
     cane1,
     cane2,
     boss;
@@ -31,18 +27,20 @@ public class Assets
      */
     public static void init()
     {
-        titlescreen = ImageLoader.loadImage("resources/images/titlescreen.png");
+        titlescreen = ImageLoader.loadImage("resources/images/title.png");
         loadingscreen = ImageLoader.loadImage("resources/images/loadingscreen.png");
         deadscreen = ImageLoader.loadImage("resources/images/dead.png");
         player = ImageLoader.loadImage("resources/images/dalelewis.png");
         idle1 = ImageLoader.loadImage("resources/images/idleanim1.png");
         idle2 = ImageLoader.loadImage("resources/images/idleanim2.png");
+        walking1 = ImageLoader.loadImage("resources/images/walking1.png");
+        walking2 = ImageLoader.loadImage("resources/images/walking2.png");
         flipidle1 = ImageLoader.loadImage("resources/images/idleanimflip1.png");
         flipidle2 = ImageLoader.loadImage("resources/images/idleanimflip2.png");
         lvl1bg = ImageLoader.loadImage("resources/images/lvl1bg.png");
-        lvl2bg = ImageLoader.loadImage("resources/images/level2bg.png");
-        lvl3bg = ImageLoader.loadImage("resources/images/level3bg.png");
-        bossbg = ImageLoader.loadImage("resources/images/bossbg.png");
+        lvl2bg = ImageLoader.loadImage("resources/images/lvl1bg.png");
+        lvl3bg = ImageLoader.loadImage("resources/images/lvl1bg.png");
+        bossbg = ImageLoader.loadImage("resources/images/lvl1bg.png");
         gameover = ImageLoader.loadImage("resources/images/lvl1bg.png");
         instructions = ImageLoader.loadImage("resources/images/instruct.png");
         slime1 = ImageLoader.loadImage("resources/images/slime1.png");
